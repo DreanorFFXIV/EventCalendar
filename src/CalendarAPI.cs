374fd4926ec1a0e3bae4ef767b052e61979b8dd9
Dreanor <mikeundead@googlemail.com>
2022-12-12 13:21:01 +0100

    added calendar

---
+++ src/CalendarAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+
+namespace EventCalendar
+{
+    public class CalendarAPI
+    {
+        private EventsResource.ListRequest Request { get; }
+        
+        public CalendarAPI()
+        {
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyBgvc495CAKUdRpocz33pttO_q2JigeC6M",
+                ApplicationName = "FFXIVEVENTS",
+            });
+            
+            Request = service.Events.List("5de3023ee82a703ccd7c4b71f0e34418967b28f0001cdb722cb261e11d51dc8d@group.calendar.google.com");
+            Request.TimeMin = DateTime.Now;
+            Request.ShowDeleted = false;
+            Request.SingleEvents = true;
+            Request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+        }
+
+        public Events GetEvents()
+        {
+            return Request.Execute();
+        }
+    }
+}
--- src/PluginUI.cs
+++ src/PluginUI.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+using System.Numerics;
+using Google.Apis.Calendar.v3.Data;
 using ImGuiNET;
 
 namespace EventCalendar
 {
     public class PluginUI : IDisposable
     {
-        private const string PublicCalendarId =
-            "5de3023ee82a703ccd7c4b71f0e34418967b28f0001cdb722cb261e11d51dc8d@group.calendar.google.com";
-        
         // this extra bool exists for ImGui, since you can't ref a property
         private bool _visible;
 
@@ -17,27 +16,72 @@ namespace EventCalendar
             set => _visible = value;
         }
 
+        private CalendarAPI CalendarAPI { get; }
+        
         public PluginUI()
         {
+            CalendarAPI = new CalendarAPI();
         }
 
         public void Draw()
         {
-            DrawCalendarEntry("title", "desc", "url");
+            if (!Visible)
+            {
+                return;
+            }
+            
+            ImGui.SetNextWindowSize(new Vector2(1000, 500), ImGuiCond.FirstUseEver);
+            if (ImGui.Begin("Events", ref _visible, ImGuiWindowFlags.None))
+            {
+                ImGui.Spacing();
+
+                if (ImGui.BeginTable("table", 4, ImGuiTableFlags.RowBg | ImGuiTableFlags.Borders | ImGuiTableFlags.BordersInner))
+                {
+                    ImGui.TableHeader("Events");
+                    ImGui.TableSetupColumn("Title", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableSetupColumn("Description", ImGuiTableColumnFlags.WidthStretch);
+                    ImGui.TableSetupColumn("Starts", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableSetupColumn("Ends", ImGuiTableColumnFlags.WidthFixed);
+                    ImGui.TableHeadersRow();
+                    ImGui.TableNextRow();
+
+                    var events = CalendarAPI.GetEvents();
+                    foreach (var eventItem in events.Items)
+                    {
+                        DrawCalendarEntry(eventItem.Summary, eventItem.Description, eventItem.Start, eventItem.End);
+                    }
+
+                    ImGui.EndTable();
+                }
+
+                ImGui.End();
+            }
         }
 
-        private void DrawCalendarEntry(string title, string description, string imgUrl)
+        private void DrawCalendarEntry(string title, string description, EventDateTime starts, EventDateTime ends)
         {
             ImGui.TableNextColumn();
-
-            if (description.Length > 200)
-            {
-                description = description[..200] + "...";
-            }
-
+            //SetCellColor();
+            ImGui.Text(title);
+            
+            ImGui.TableNextColumn();
             ImGui.TextWrapped(description);
-
+            
+            ImGui.TableNextColumn();
+            //SetCellColor();
+            ImGui.Text(string.IsNullOrWhiteSpace(starts.Date) ? starts.DateTime.ToString() : starts.Date);
+            
+            ImGui.TableNextColumn();
+            //SetCellColor();
+            ImGui.Text(string.IsNullOrWhiteSpace(ends.Date) ? ends.DateTime.ToString() : ends.Date);
+            
             ImGui.TableNextRow();
+        }
+
+        private void SetCellColor()
+        {
+            var greenColor = new Vector4(0.0742f, 0.530f, 0.150f, 1.0f);
+            ImGui.TableSetBgColor(ImGuiTableBgTarget.CellBg, ImGui.ColorConvertFloat4ToU32(greenColor));
         }
 
         public void Dispose()
